145b2cf453817e4074b8e4297e988b22ebd7a6b2
Payton Harris <paytonharris000@gmail.com>
2021-11-03 17:56:42 -0400

    update splash screen with links, more styles, and correct css for header

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,48 @@
 import React from 'react';
-import profilepic from './assets/profilepic.jpg'
+import { profilepic } from './assets';
+import { Instagram, GitHub, Mail, Linkedin } from 'react-feather';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="Splash-page">
-        <div className="About-me">
-          <h1 className="Card-title">
-            Payton Harris
+    <div className="app">
+      <div className="splash-page background-highlight-down">
+        <div className="about-me">
+          <h1 className="card-title">
+            About Me
           </h1>
-          <div className="About-me-left">
-            <p>Hello,</p>
-            <p>I'm Payton, a Software Engineer</p>
-            <div className="Divider" />
-            <p className="Additional-info">iOS Developer; React Native; Musician</p>
-            <p className="Additional-info">Creator of Unheard Noise, and more projects below.</p>
-          </div>
-          <div className="About-me-right">
-            <img src={profilepic} className="Profile-pic" alt="payton" />
+          <div className="about-me-box">
+            <div className="about-me-left">
+              <div className="about-me-main-info">
+                <p>Hello,</p>
+                <p>I'm Payton, a Software Engineer</p>
+                <div className="divider" />
+                <p className="additional-info">iOS Swift; React Native; Musician</p>
+                <p className="additional-info">App developer who loves front-end polish, with 4 years experience across app development and full stack web development.</p>
+              </div>
+              <div className="link-grid">
+                <a href="https://github.com/paytonharris" className="link-icon">
+                  <GitHub color="white" size={32} strokeWidth={1} />
+                </a>
+                <a href="https://www.linkedin.com/in/payton-b-harris/" className="link-icon">
+                  <Linkedin color="white" size={32} strokeWidth={1} />
+                </a>
+                <a href="https://www.instagram.com/payton.b.harris/" className="link-icon">
+                  <Instagram color="white" size={32} strokeWidth={1} />
+                </a>
+                <a href="mailto:payton@paytonharris.com" className="link-icon">
+                  <Mail color="white" size={32} strokeWidth={1} />
+                </a>
+              </div>
+            </div>
+            <div className="about-me-right">
+              <img src={profilepic} className="profile-pic" alt="payton" />
+            </div>
           </div>
         </div>
       </div>
-      <div className="Splash-page">
-        <div className="About-me">
+      <div className="splash-page background-basic">
+        <div className="about-me">
           <h1>
             More details
           </h1>
